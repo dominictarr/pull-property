5603fb0a821ab01b1d65377c651553ce1aef5337
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-01 01:06:56 +0200

    better tests

--- test/index.js
+++ test/index.js
@@ -44,14 +44,51 @@ tape('update value overwrites it', function (t) {
 tape('unset first property', function (t) {
   var read = property()
 
-  var called = false
+  var called = false, called2 = false
   read(null, function (err, data) {
     called = true
     if(err) throw err
     t.equal(data, 5)
-    t.end()
+    read(null, function (end, data) {
+      called2 = true
+      t.equal(end, true)
+      t.end()
+    })
+
+    t.equal(called2, false)
+    read.end()
   })
   t.equal(called, false)
   read.update(5)
 
 })
+
+
+function delay () {
+  return function (read) {
+    return function (abort, cb) {
+      setTimeout(function () {
+        read(abort, cb)
+      }, 10)
+    }
+  }
+}
+
+tape('property2', function (t) {
+
+  var p = property(3)
+  pull(p, delay(), pull.collect(function (err, ary) {
+    if(err) throw err
+    t.deepEqual(ary, [2])
+    t.end()
+  }))
+
+  setTimeout(function () {
+    p.update(5)
+    p.update(3)
+    p.update(2)
+    p.end()
+  })
+
+})
+
